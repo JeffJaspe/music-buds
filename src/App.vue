18aaa14bb0fbdec2af640ffed0c96b2b660977d7
Jeff Jaspe <jeffreyjoyjaspe@gmail.com>
2025-08-03 04:55:59 +0000

    fix bug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="p-6 bg-gray-900 min-h-screen text-white">
-    <h1 class="text-3xl font-bold mb-6">Top 20 Dota 2 Players with Highest Win Streak</h1>
-    <TopPlayers />
+    <MatchResult />
   </div>
 </template>
 
